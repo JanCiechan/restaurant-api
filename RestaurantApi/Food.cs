da63478c51b17cc990a6eea9aa705db4022036f2
JanCiechan <123jan321ciechan@gmail.com>
2023-07-18 11:36:17 +0200

    food model

---
+++ RestaurantApi/Food.cs
@@ -0,0 +1,15 @@
+﻿namespace RestaurantApi
+{
+    public class Food
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public bool Vegan { get; set; }
+
+
+    }
+}
