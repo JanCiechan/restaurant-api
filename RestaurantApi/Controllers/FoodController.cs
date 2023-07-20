3133d290990b552a6ea3bbaff48f72c2289337be
JanCiechan <123jan321ciechan@gmail.com>
2023-07-20 08:39:27 +0200

    added basic crud

--- RestaurantApi/Controllers/FoodController.cs
+++ RestaurantApi/Controllers/FoodController.cs
@@ -14,8 +14,46 @@ namespace RestaurantApi.Controllers
         [HttpGet]
         public async Task<IEnumerable<Food>> Get()
         {
-            return await _foodDbContext.Foods.ToListAsync(); 
+            return await _foodDbContext.Foods.ToListAsync();
+        }
+        [HttpGet("id")]
+        [ProducesResponseType(typeof(Food), 200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var food = await _foodDbContext.Foods.FindAsync(id);
+            return food == null ? NotFound() : Ok(food);
         }
 
+        [HttpPost]
+        [ProducesResponseType(201)]
+        public async Task<IActionResult> Create(Food food)
+        {
+            await _foodDbContext.Foods.AddAsync(food);
+            await _foodDbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = food.Id }, food);
+        }
+        [HttpPut("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Update(int id,Food food)
+        {
+            if(id!= food.Id) return BadRequest();
+            _foodDbContext.Entry(food).State = EntityState.Modified;
+            await _foodDbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(204)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var foodToDelete= await _foodDbContext.Foods.FindAsync(id);
+            if (foodToDelete == null) return NotFound();
+            _foodDbContext.Foods.Remove(foodToDelete);
+            await _foodDbContext.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
